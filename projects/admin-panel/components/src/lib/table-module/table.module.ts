6015acf7a9e733185923431a4a87b6b019033ae6
alejandro camba <alejandrocamba@MacBook-Pro-de-alejandro.local>
2020-07-07 13:27:15 -0400

    removing table code

--- projects/admin-panel/components/src/lib/table-module/table.module.ts
+++ projects/admin-panel/components/src/lib/table-module/table.module.ts
@@ -2,14 +2,13 @@ import { NgModule } from '@angular/core';
 import { BlockModule } from '@admin-panel/core';
 import { TableComponent } from './table.component';
 import { CommonModule } from '@angular/common';
-import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { ButtonComponent } from '../independent-module/components/button.component';
 import { DropdownComponent } from '../independent-module/components/dropdown.component';
 import { LabelComponent } from '../independent-module/components/label.component';
 import { IndependentModule } from '../independent-module/independent.module';
 
 @NgModule({
-  imports: [CommonModule, IndependentModule, NgxDatatableModule, BlockModule],
+  imports: [CommonModule, IndependentModule, BlockModule],
   declarations: [TableComponent],
   entryComponents: [ButtonComponent, DropdownComponent, LabelComponent],
   bootstrap: [],
